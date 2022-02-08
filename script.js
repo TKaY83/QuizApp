6786f659cd3aafe0158d085d6eb3f24f1027a555
Tuncay Dagdelen <sramos083@gmail.com>
2022-02-08 13:48:42 +0100

    alpha 1.0

--- script.js
+++ script.js
@@ -1,108 +1,77 @@
-let questions = [
-    {
-        "question": "Wer hat HTML erfunden?",
-        "answer_1": "Robbie Williams",
-        "answer_2": "Lady Gaga",
-        "answer_3": "Tim Berners-Lee",
-        "answer_4": "Justin Bieber",
-        "right-answer": 3
-    },
-    {
-        "question": "Was bedeutet das HTML Tag &lt;a&gt;?",
-        "answer_1": "Text Fett",
-        "answer_2": "Container",
-        "answer_3": "Ein Link",
-        "answer_4": "Kursiv",
-        "right-answer": 3
-    },
-    {
-        "question": "Wie bindet man eine Website in deine Website ein?",
-        "answer_1": "&lt;iframe&gt;, &ltframe&gt;, and &lt;frameset&gt;",
-        "answer_2": "&lt;iframe&gt;",
-        "answer_3": "&lt;frame&gt;",
-        "answer_4": "&lt;frameset&gt;",
-        "right-answer": 2
-    },
-    {
-        "question": "Wie stellt man Text am BESTEN fett dar?",
-        "answer_1": "&lt;strong&gt;",
-        "answer_2": "CSS nutzen",
-        "answer_3": "&lt;sbold&gt;",
-        "answer_4": "&lt;b&gt;",
-        "right-answer": 1
-    },
-    {
-        "question": "Welches Attribut kann man NICHT für Textarea verwenden?",
-        "answer_1": "readonly",
-        "answer_2": "max",
-        "answer_3": "form",
-        "answer_4": "spellcheck",
-        "right-answer": 1
-    },
-    {
-        "question": "Wie wählst du alle Elemente vom Typ &lt;a&gt; mit dem attribut title aus?",
-        "answer_1": "a[title] {...}",
-        "answer_2": "a > title {...}",
-        "answer_3": "a.title {...}",
-        "answer_4": "a=title {...}",
-        "right-answer": 1
-    },
-    {
-        "question": "Wie definierst du in JavaSript eine Variable?",
-        "answer_1": "let 100 = rate;",
-        "answer_2": "100 = let rate;",
-        "answer_3": "rate = 100;",
-        "answer_4": "let rate = 100;",
-        "right-answer": 4
-    },
-];
+let rightAnwsers = [];
+let currentQuestion = 0;
+let AUDIO_SUCCESS = new Audio('./sounds/right.mp3');
+let AUDIO_FAIL = new Audio('./sounds/wrong.mp3');
+let FINAL_SOUND = new Audio('./sounds/tada.mp3');
+// let BACKGROUND_SOUND = new Audio('./sounds/background.mp3');
 
-let currentQuestion = 0; //die 1. (also 0.) frage wird aus dem json geholt
 
-function render(){//wird beim fertigladen des html geladen
-    document.getElementById('all-questions').innerHTML = questions.length;//zeigt die gesamtanzahl der fragen des arrays auf der card an 7
+function render() {
+    document.getElementById('all-questions').innerHTML = questions.length;
     showQuestion();
 }
 
-function showQuestion(){// wird auch geladen nachdem das html geladen wurde zeigt die frage und antworten an
 
-    if (currentQuestion >= questions.length){
-        //TODO: Show End Screen
-    } else{
-        let question = questions[currentQuestion]; // hier wird die 0. stelle des json definiert, also die 1. frage
-        document.getElementById('current-question-number').innerHTML = currentQuestion + 1;
-        document.getElementById('question-text').innerHTML = question['question'];//hier werden die jeweiligen fächer aus dem json 0 angezeigt 
-        document.getElementById('answer_1').innerHTML = question['answer_1'];
-        document.getElementById('answer_2').innerHTML = question['answer_2'];
-        document.getElementById('answer_3').innerHTML = question['answer_3'];
-        document.getElementById('answer_4').innerHTML = question['answer_4'];
+function showQuestion() {
+    if (gameIsOver()) {
+        showEndScreen();
+        if (allIsRight()) {
+            allRight();
+        }
+    } else {
+        updateProgressBar();
+        showNextQuestion();
     }
 }
 
-function answer(selection){ // die per onklick function eingegebe parameter werden hier in die variable selection gespeichert
-    let question = questions[currentQuestion];// wie oben 
-    let selectedQuestionNumber = selection.slice(-1);//das letzte zeichen des strings wird abgerufen
+function allRight(){
+    document.getElementById('all-right').innerHTML = `<b>Großartig</b>, du hast alle fragen <b>Richtig</b> beantwortet!"`;
+
+}
+
+
+function allIsRight(){
+    return rightAnwsers.length == 7;
+}
+
+
+function gameIsOver(){
+    return currentQuestion >= questions.length;
+}
+
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1);
     let idOfRightAnswer = `answer_${question['right-answer']}`;
 
-    if (selectedQuestionNumber == question['right-answer']){// wenn letzte zahl in der variable selection 3 und die im json gespeicherte infomation auch 3 ist
-        document.getElementById(selection).parentNode.classList.add('bg-success');//parentNode greift auf das übergeordnete element zu
-    } else{
+    if (rightAnswerSelected(selectedQuestionNumber, question)) {
+        document.getElementById(selection).parentNode.classList.add('bg-success');
+        rightAnwsers.push(1);
+        AUDIO_SUCCESS.play();
+    } else {
         document.getElementById(selection).parentNode.classList.add('bg-danger');
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
+        AUDIO_FAIL.play();
     }
-    document.getElementById('next-button').disabled = false; //hebt das disablet attribut auf
+    document.getElementById('next-button').disabled = false;
 }
 
-function nextQuestion(){
+
+function rightAnswerSelected(sQN, question){
+    return sQN == question['right-answer']
+}
+
+
+function nextQuestion() {
     currentQuestion++;
     document.getElementById('next-button').disabled = true;
     removeDangerSuccess();
     showQuestion();
-
-
 }
 
-function removeDangerSuccess(){
+
+function removeDangerSuccess() {
     document.getElementById('answer_1').parentNode.classList.remove('bg-danger');
     document.getElementById('answer_1').parentNode.classList.remove('bg-success');
     document.getElementById('answer_2').parentNode.classList.remove('bg-danger');
@@ -111,4 +80,45 @@ function removeDangerSuccess(){
     document.getElementById('answer_3').parentNode.classList.remove('bg-success');
     document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
     document.getElementById('answer_4').parentNode.classList.remove('bg-success');
+}
+
+
+function restartGame() {
+    document.getElementById('end-picture').src = './img/question.jpg'
+    document.getElementById('question-body').style.display = '';
+    document.getElementById('end-screen').style.display = 'none';
+    rightAnwsers = [];
+    currentQuestion = 0;
+    render();
+}
+
+
+function showEndScreen() {
+    document.getElementById('end-screen').style = '';
+    document.getElementById('question-body').style.display = 'none';
+    document.getElementById('total-right-answers').innerHTML = rightAnwsers.length;
+    document.getElementById('total-questions-end-screen').innerHTML = questions.length;
+    document.getElementById('end-picture').src = './img/employee.jpg'
+    document.getElementById('progress-bar').innerHTML = `100 %`;
+    document.getElementById('progress-bar').style = `width: 100%`;
+    FINAL_SOUND.play();
+}
+
+
+function updateProgressBar() {
+    let percent = currentQuestion / questions.length;
+    percent = Math.round(percent * 100);
+    document.getElementById('progress-bar').innerHTML = `${percent} %`;
+    document.getElementById('progress-bar').style.width = `${percent}%`;
+}
+
+
+function showNextQuestion() {
+    let question = questions[currentQuestion]; // hier wird die 0. stelle des json definiert, also die 1. frage
+    document.getElementById('current-question-number').innerHTML = currentQuestion + 1;
+    document.getElementById('question-text').innerHTML = question['question'];//hier werden die jeweiligen fächer aus dem json 0 angezeigt 
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
 }
